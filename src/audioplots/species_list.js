bb1eb2bcc1654ee0ea0ee550701fddad1affc93d
mbsantiago <santiago.mbal@gmail.com>
2018-09-06 17:13:23 -0500

    Modified list for changes in input.

--- src/audioplots/species_list.js
+++ src/audioplots/species_list.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import './species_list.css';
 
 
 class SpeciesList extends Component {
@@ -14,47 +15,122 @@ class SpeciesList extends Component {
   }
 
   processData() {
-    const year_totals = this.data.year_totals[this.state.year];
     const listed_species = this.data.listed_species;
-    let detected_data = year_totals.species_activity;
 
-    let detected_species = Object.keys(detected_data)
-      .reduce(function (filtered, key) {
-        if (detected_data[key] > 0) filtered[key] = detected_data[key];
-        return filtered;
-      }, {});
-
-    let data = {};
-
-    for (var key in detected_species) {
-      if (listed_species.indexOf(key) < 0) {
-        data[key] = [detected_species[key], 'table-success'];
-      } else {
-        data[key] = [detected_species[key], 'table-default'];
-      }
-    }
+    const year_totals = this.data.year_totals[this.state.year];
+    const detected_data = year_totals.species_activity.mean;
+    const all_species = Object.keys(detected_data);
 
     for (var i = 0; i < listed_species.length; i++) {
-      let species = listed_species[i];
-      if (!(species in data)) {
-        data[species] = [0, 'table-warning'];
+      if (all_species.indexOf(listed_species[i]) < 0) {
+        console.log('Unknown listed species');
       }
     }
 
+    let all_data = {};
+    for (var j = 0; j < all_species.length; j++) {
+      for (var k = 0; k < this.years.length; k ++) {
+        let year = this.years[k];
+        let species = all_species[j];
+        let mean_value = this.data.year_totals[year].species_activity.mean[species];
+        let q25_value = this.data.year_totals[year].species_activity.q25[species];
+        let q75_value = this.data.year_totals[year].species_activity.q75[species];
+
+        if (!(species in all_data)) {
+          let is_listed = (listed_species.indexOf(species) >= 0);
+          all_data[species] = {is_listed: is_listed};
+        }
+        all_data[species][year] = {q25: q25_value, q75: q75_value, mean: mean_value};
+      }
+    }
+
+    let data = Object.keys(all_data).reduce(
+      (filtered, key) => {
+        if (all_data[key].is_listed) {
+          filtered[key] = all_data[key];
+          return filtered;
+        }
+        for (var l = 0; l < this.years.length; l++) {
+          if (all_data[key][this.years[l]].mean > 0) {
+            filtered[key] = all_data[key];
+            return filtered;
+          }
+        }
+
+        return filtered;
+      }, {});
     return data;
+  }
+
+  getCounts(data) {
+    let counts = {};
+
+    for (var i=0; i < this.years.length; i++) {
+      let year = this.years[i];
+      counts[year] = {};
+
+      let num_listed = 0;
+      let num_unlisted = 0;
+      for (var species in data) {
+        if (data[species][year] > 0) {
+          if (data[species].is_listed) {
+            num_listed++;
+          } else {
+            num_unlisted++;
+          }
+        }
+      }
+    }
+
   }
 
   buildTable() {
     let data = this.processData();
 
     let items = Object.keys(data).sort();
+
+    let get_year_rows = (species) => {
+      let rows = this.years.map(
+        (year) => {
+
+          return ['q25', 'mean', 'q75'].map(
+            (key) => {
+              let className;
+              let value = data[species][year][key];
+
+              if (data[species].is_listed) {
+                className = value === 0 ? 'text-info' : '';
+              } else {
+                className = value === 0 ? '' : 'text-success';
+              }
+
+              return <td key={year + species + key} className={className}>{value}</td>;
+            }
+          );
+        }
+      );
+      return rows;
+    };
+
     let table_rows = items.map(
       (species, i) => (
-        <tr className={data[species][1]}>
+        <tr key={'row' + species} className={data[species].is_listed ? 'table-default' : 'table-secondary'}>
           <th scope="row">{i + 1}</th>
-          <td>{species}</td>
-          <td>{data[species][0]}</td>
+          <th>{species}</th>
+          {get_year_rows(species)}
         </tr>
+      )
+    );
+
+    let headers = this.years.map(
+      (year) => <th scope="col" key={'col' + year} colspan={3}>{year}</th>
+    );
+
+    let subheaders = this.years.map(
+      (year) => (
+          ['25%', 'media', '75%'].map(
+            (type) => <td scope="col" key={'col' + year + type}>{type}</td>
+          )
       )
     );
 
@@ -62,10 +138,11 @@ class SpeciesList extends Component {
       <table className="table table-hover">
         <thead className="thead-dark">
           <tr>
-            <th scope="col">#</th>
-            <th scope="col">Especie</th>
-            <th scope="col">Detecciones por Día (promedio)</th>
+            <th scope="col" rowspan={2}>#</th>
+            <th scope="col" rowspan={2}>Especie</th>
+            {headers}
           </tr>
+          {subheaders}
         </thead>
         <tbody>
           {table_rows}
@@ -93,16 +170,11 @@ class SpeciesList extends Component {
     );
   }
 
-  handleYearClick(year) {
-    this.setState({year: year});
-  }
-
   render() {
     let table = this.buildTable();
 
     return (
       <div className='plot'>
-        {this.getButtons()}
         {table}
       </div>);
   }
